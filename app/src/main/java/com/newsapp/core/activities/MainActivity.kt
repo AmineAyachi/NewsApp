00e62e7ab63de45faac99d2ea757e0a0b50689bf
amine <ayachi.m.amine@gmail.com>
2023-04-19 11:48:26 +0200

    Setup Dagger hilt et network security

--- app/src/main/java/com/newsapp/core/activities/MainActivity.kt
+++ app/src/main/java/com/newsapp/core/activities/MainActivity.kt
@@ -1,43 +1,39 @@
 package com.newsapp.core.activities
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.newsapp.ui.theme.NewsAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
+@ExperimentalAnimationApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NewsAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+
+                val activity = LocalContext.current as Activity
+                val navController = rememberAnimatedNavController()
+                var navigateClick by remember { mutableStateOf(false) }
+               }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NewsAppTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/newsapp/core/activities/view_models/MainViewModel.kt
@@ -0,0 +1,15 @@
+package com.newsapp.core.activities.view_models
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel  @Inject constructor(
+    private @ApplicationContext val context: Context,
+)  : ViewModel(){
+
+
+}
